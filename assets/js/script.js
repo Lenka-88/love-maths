aff0757827fd8245f52931d2e5bffe06a5a6def3
Lenka Fekulova <lenka.fekulova88@gmal.com>
2024-12-28 22:02:43 +0100

    JS numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert("you clicked ${gameType}");
+      }
+    });
+  }
+});
+
+
+
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
 
-}
+// Creates two random numbers between 1 and 25
+let num1 = Math.floor(math.random() * 25) + 1;
+let num2 = Math.floor(math.random() * 25) + 1;
+]
 
-function checkAnswer() {
+function checkAnswer() {}
 
-}
+function calculateCorrectAnswer() {}
 
-function calculateCorrectAnswer() {
+function incrementScore() {}
 
-}
+function incrementWrongAnswer() {}
 
-function incrementScore() {
+function displayAdditionQuestion() {}
 
-}
+function displaySubtractQuestion() {}
 
-function incrementWrongAnswer() {
-
-}
-
-function displayAdditionQuestion() {
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-    
-}
+function displayMultiplyQuestion() {}
